21d142a425ccf8929f05a85a316e0c41f503177b
caoshiqing <1043369832@qq.com>
2025-11-21 15:35:06 +0800

    feat: 修改sandbox环境搭建5

--- docker_helm_utils/change_urls.sh
+++ docker_helm_utils/change_urls.sh
@@ -9,5 +9,10 @@ fi
 if [ -n "$envLogsUrl" ]; then
   sed -i -e "s|https://sandbox.hyperswitch.io/logs/sdk|${envLogsUrl}|g" app.js HyperLoader.js
 fi
+
+if [ -f "app.js" ]; then
+  NEW_HASH=$(openssl dgst -sha384 -binary app.js | openssl base64 -A)
+  sed -i -e "s|integrity=\"sha384-[^\"]*\"|integrity=\"sha384-${NEW_HASH}\"|g" *.html
+fi
 echo "Build modifications completed."
 exec "$@"
