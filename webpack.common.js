3f1aefb9e840548c70df25e77f88fdf39bca47e5
caoshiqing <1043369832@qq.com>
2025-11-24 11:46:16 +0800

    feat: 修改sandbox环境搭建12

--- webpack.common.js
+++ webpack.common.js
@@ -21,6 +21,7 @@ const localhostSources = [
   "http://127.0.0.1:3103",
   "http://127.0.0.1:5252",
   "https://192.168.30.127:8080",
+  "https://192.168.30.127:3104",
 ];
 
 // List of authorized external script sources (for Content Security Policy)
