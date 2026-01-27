49132cd6b32f2df6d13b17f3a52a8d52a1ac7204
caoshiqing <1043369832@qq.com>
2026-01-27 15:47:29 +0800

    feat:修改hyperswitch-web的构建文件

--- docker_helm_utils/change_urls.sh
+++ docker_helm_utils/change_urls.sh
@@ -17,5 +17,12 @@ if [ -f "app.js" ]; then
   sed -i -e "s|\(<script[^>]*app\.js[^>]*\)integrity=\"sha384-[a-zA-Z0-9+/=]*\"|\1integrity=\"sha384-${NEW_HASH}\"|g" *.html
 fi
 
+# 重新计算并更新 HyperLoader.js 的 integrity 值
+if [ -f "HyperLoader.js" ]; then
+  LOADER_HASH=$(openssl dgst -sha384 -binary HyperLoader.js | openssl base64 -A)
+  # 更新包含 HyperLoader.js 的行中的 integrity
+  sed -i -e "s|\(<script[^>]*HyperLoader\.js[^>]*\)integrity=\"sha384-[a-zA-Z0-9+/=]*\"|\1integrity=\"sha384-${LOADER_HASH}\"|g" *.html
+fi
+
 echo "Build modifications completed."
 exec "$@"
